2f0dfc19be022ae89aa182be2d7a97e70bbee2dc
vityacasf <neustupov01@mail.ru>
2022-07-12 20:00:50 +0300

    homework lesson2 done

---
+++ src/com/courses/lesson2/Calculations.java
@@ -0,0 +1,15 @@
+package com.courses.lesson2;
+
+public class Calculations
+{
+    public static void main(String[] args)
+    {
+        Operations operation = new Operations();
+        operation.adding();
+        operation.subtracting();
+        operation.division();
+        operation.GetResidue();
+        operation.ExpressionSolution();
+        operation.TestLogicalOperators();
+    }
+}
